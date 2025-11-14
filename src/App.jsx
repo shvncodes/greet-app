cbf870668c26a036729d2e69492d9e72e214b5a2
Shivani Choudhary <shivanichoudhary2206@gmail.com>
2025-11-14 19:37:54 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Greet from "./greet"
+
+function App() {
+    return (
+        <>
+            <Greet name={"Shivani"}/>
+            <Greet name={"Nisha"}/>
+            <Greet name={"Ruhi"}/>
+            <Greet name={"Akansha"}/>
+            <Greet name={"Simran"}/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/greet.jsx
@@ -0,0 +1,11 @@
+function Greet(props) {
+    const {name} = props;
+
+    return (
+        <>
+        <h1>Hello {name}:)</h1>
+        </>
+    )
+}
+
+export default Greet
